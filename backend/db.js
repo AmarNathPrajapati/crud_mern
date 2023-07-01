837728bc4c3b873e6c6b725aef574095b49dd773
amarnath <amarnathjnp2002@gmail.com>
2023-07-01 13:18:48 +0530

    in crud app deletion, creation and read operation completed

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const mongorURI = "mongodb://127.0.0.1:27017/crud_app";
+const connectToDb = async() =>{
+    await mongoose.connect(mongorURI).then(()=>{
+        console.log('Connected to MongoDB');
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+module.exports = connectToDb;
